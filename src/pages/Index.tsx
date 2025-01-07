e6bad80fd37a01ba22b4339f012595336b0c417f
Mayank Gupta <goyalmayank300@gmail.com>
2025-01-08 03:23:54 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import { Hero } from "@/components/Hero";
 import { RecommendationSection } from "@/components/RecommendationSection";
 
@@ -9,6 +10,7 @@ const Index = () => {
       <main>
         <Hero />
         <RecommendationSection />
+        <Footer />
       </main>
     </div>
   );
